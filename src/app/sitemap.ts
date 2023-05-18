70779e8383e824f0af954206308bf90ecda0277d
Marco Baiad <marcobaiad@gmail.com>
2023-05-17 22:33:15 -0300

    change public env to normal

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,18 +6,20 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const provinces: string[] = await getCodesOnlyService('Provincia')
 
   const areaCodes = codes.map((areaCode) => ({
-    url: `${process.env.NEXT_PUBLIC_API_URL}/codigo-de-area/${areaCode}`,
+    url: `${
+      process.env.NEXT_PUBLIC_API_URL || process.env.API_URL
+    }/codigo-de-area/${areaCode}`,
     lastModified: new Date(),
   }))
   const states = provinces.map((province) => ({
-    url: `${process.env.NEXT_PUBLIC_API_URL}/provincia/${stringToSlug(
-      normalizeString(province)
-    )}`,
+    url: `${
+      process.env.NEXT_PUBLIC_API_URL || process.env.API_URL
+    }/provincia/${stringToSlug(normalizeString(province))}`,
     lastModified: new Date(),
   }))
 
   const routes = ['', '/provincia'].map((route) => ({
-    url: `${process.env.NEXT_PUBLIC_API_URL}${route}`,
+    url: `${process.env.NEXT_PUBLIC_API_URL || process.env.API_URL}${route}`,
     lastModified: new Date(),
   }))
 
--- src/services/getCodesOnly.ts
+++ src/services/getCodesOnly.ts
@@ -1,6 +1,8 @@
 export const getCodesOnlyService = async (prefijo: string) => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/area-codes/only?${prefijo}=""`
+    `${
+      process.env.NEXT_PUBLIC_API_URL || process.env.API_URL
+    }/api/area-codes/only?${prefijo}=""`
   )
   const data = await res.json()
   if (!res.ok) throw data
