72f489b01b054b5ad985da4fc1f4e057c36d2607
Marco Baiad <marcobaiad@gmail.com>
2023-05-30 20:37:32 -0300

    add tag manager

--- src/components/Analytics/Analytics.tsx
+++ src/components/Analytics/Analytics.tsx
@@ -11,28 +11,35 @@ declare const window: WindowWithDataLayer
 
 // window.dataLayer = window.dataLayer || []
 
-export const GTMscript = () => (
-  <>
-    <Script id="window-dataLayer">{`window.dataLayer = window.dataLayer || [];`}</Script>
-    <Script id="google-tag-manager" strategy="afterInteractive">
-      {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', 'G-N4YHRHCP99')
-    `}
-    </Script>
-  </>
-)
+export function GTMscript() {
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-N4YHRHCP99"
+      />
+      <Script id="google-tag-manager">
+        {`
+          window.dataLayer = window.dataLayer || []
+          function gtag(){
+            window.dataLayer.push(arguments)
+          }
+          gtag('js', new Date());
 
-export const GTMnoscript = () => (
-  <noscript
-    dangerouslySetInnerHTML={{
-      __html: `
-      <iframe src="https://www.googletagmanager.com/ns.html?id=G-N4YHRHCP99"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    `,
-    }}
-  />
-)
+          gtag('config', 'G-N4YHRHCP99')
+        `}
+      </Script>
+    </>
+  )
+}
+
+// export const GTMnoscript = () => (
+//   <noscript
+//     dangerouslySetInnerHTML={{
+//       __html: `
+//       <iframe src="https://www.googletagmanager.com/ns.html?id=G-N4YHRHCP99"
+//       height="0" width="0" style="display:none;visibility:hidden"></iframe>
+//     `,
+//     }}
+//   />
+// )
